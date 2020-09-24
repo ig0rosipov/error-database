f36f95db2d2660335c7df18039274f8a98a89ea7
Igor Osipov <igor.osipov@modern.org>
2020-09-24 15:12:08 +0300

    feat: form layout

---
+++ scripts/popup.js
@@ -0,0 +1,8 @@
+// const setPopupListeners = () => {
+//   const popups = Array.from(document.querySelectorAll('.popup'));
+//   popups.forEach(popup => {
+//     popup.addEventListener('click')
+//   })
+// }
+
+// setPopupListeners();
