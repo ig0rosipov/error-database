9a8be0a771abbc57c78383bac8944cb16386faeb
Igor Osipov <igor.osipov@modern.org>
2020-09-29 11:51:34 +0300

    style: popup closing button

--- scripts/popup.js
+++ scripts/popup.js
@@ -1,9 +1,9 @@
 const newErorrForm = document.querySelector('.new-error-form');
+const addMachineButton = newErorrForm.querySelector('.form__add-machine-button');
 const popup = document.querySelector('.popup');
+
 
 addMachineButton.addEventListener('click', event => {
   event.preventDefault();
   popup.classList.add('popup_opened');
 });
-
-
