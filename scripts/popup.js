060a30a90d934594c5bfb25c8f4a3dac5b867caf
Igor Osipov <igor.osipov@modern.org>
2020-09-29 09:56:31 +0300

    style: useless files deleted

--- scripts/popup.js
+++ scripts/popup.js
@@ -1,8 +1,1 @@
-// const setPopupListeners = () => {
-//   const popups = Array.from(document.querySelectorAll('.popup'));
-//   popups.forEach(popup => {
-//     popup.addEventListener('click')
-//   })
-// }
-
-// setPopupListeners();
+//qwer
