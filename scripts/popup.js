57193490dfb282f7746804062ab1f49f32a991a0
Igor Osipov <igor.osipov@modern.org>
2020-09-29 11:06:14 +0300

    feat: popup opening

--- scripts/popup.js
+++ scripts/popup.js
@@ -1,1 +1,9 @@
-//start
+const newErorrForm = document.querySelector('.new-error-form');
+const popup = document.querySelector('.popup');
+
+addMachineButton.addEventListener('click', event => {
+  event.preventDefault();
+  popup.classList.add('popup_opened');
+});
+
+
