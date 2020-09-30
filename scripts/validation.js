6dd3333f6373c056a3ef12602c412d005a4d46aa
Igor Osipov <igor.osipov@modern.org>
2020-09-30 16:52:16 +0300

    refactor: input autocomlete off

--- scripts/validation.js
+++ scripts/validation.js
@@ -2,24 +2,29 @@ const inputList = Array.from(document.querySelectorAll('.input'));
 
 
 const showError = (item) => {
-  const formField = item.parentElement.parentElement;
+  if (item.id !== 'machine-name') {
+    const formField = item.parentElement.parentElement;
+    formField.classList.add('form__field_type_error');
+  }
+
   const errorMessage = document.querySelector(`.${item.id}-error`);
   errorMessage.textContent = item.validationMessage;
   errorMessage.classList.add('input-error_displayed');
   item.classList.remove('input_type_correct');
-  formField.classList.add('form__field_type_error');
   item.classList.add('input_type_error');
 
 }
 
 const hideError = (item) => {
-  const formField = item.parentElement.parentElement;
+  if (item.id !== 'new-machine') {
+    const formField = item.parentElement.parentElement;
+    formField.classList.remove('form__field_type_error');
+  }
   const errorMessage = document.querySelector(`.${item.id}-error`);
   errorMessage.textContent = '';
   errorMessage.classList.remove('input-error_displayed');
   item.classList.remove('input_type_error');
   item.classList.add('input_type_correct');
-  formField.classList.remove('form__field_type_error');
 }
 
 const setValidationListeners = () => {
