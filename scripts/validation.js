d9188bececef8829b883c59496cb2e94cbe3461d
Igor Osipov <igor.osipov@modern.org>
2020-09-30 16:35:34 +0300

    feat: automatic validation works

--- scripts/validation.js
+++ scripts/validation.js
@@ -1,12 +1,39 @@
-const inputList = Array.from(document.querySelectorAll('input'));
+const inputList = Array.from(document.querySelectorAll('.input'));
 
 
-const setValidationListeners = () => {
+const showError = (item) => {
+  const formField = item.parentElement.parentElement;
+  const errorMessage = document.querySelector(`.${item.id}-error`);
+  errorMessage.textContent = item.validationMessage;
+  errorMessage.classList.add('input-error_displayed');
+  item.classList.remove('input_type_correct');
+  formField.classList.add('form__field_type_error');
+  item.classList.add('input_type_error');
 
 }
 
-inputList.forEach(item => {
-  item.addEventListener('input', ()=> {
+const hideError = (item) => {
+  const formField = item.parentElement.parentElement;
+  const errorMessage = document.querySelector(`.${item.id}-error`);
+  errorMessage.textContent = '';
+  errorMessage.classList.remove('input-error_displayed');
+  item.classList.remove('input_type_error');
+  item.classList.add('input_type_correct');
+  formField.classList.remove('form__field_type_error');
+}
 
-  })
-});
+const setValidationListeners = () => {
+  inputList.forEach(item => {
+    item.addEventListener('input', ()=> {
+      if (!item.validity.valid){
+        showError(item);
+      } else {
+        hideError(item);
+      }
+
+    })
+  });
+}
+
+
+setValidationListeners();
