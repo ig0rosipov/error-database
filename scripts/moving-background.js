f2334825afd08aa019d08b07882b65ec75cd3a6b
Igor Osipov <igor.osipov@modern.org>
2020-10-06 16:40:18 +0300

    feat: alive background

---
+++ scripts/moving-background.js
@@ -0,0 +1,7 @@
+const background = document.querySelector('.root__background');
+
+document.addEventListener("mousemove", (e) => {
+  console.log(e);
+  background.style.backgroundPositionX = -e.x*0.2 + "px";
+  background.style.backgroundPositionY = -e.y*0.2 + "px";
+});
