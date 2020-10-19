728efce1d7823b61bac779689137a26d087e6f79
Igor Osipov <igor.osipov@modern.org>
2020-10-19 12:09:57 +0300

    refactor: background moving algorythm

--- scripts/moving-background.js
+++ scripts/moving-background.js
@@ -1,6 +1,7 @@
 const background = document.querySelector('.root__background');
 
 document.addEventListener("mousemove", (e) => {
-  background.style.backgroundPositionX = -e.x*0.2 + "px";
-  background.style.backgroundPositionY = -e.y*0.2 + "px";
+  background.style.backgroundPositionX = 20 * (Math.max(-100, Math.min(100, window.innerWidth) / 2 - e.x)) / 100 + "px";
+  background.style.backgroundPositionY = 20 * (Math.max(-100, Math.min(100, window.innerHeight) / 2 - e.y)) / 100 + "px";
+  console.log(background.style.backgroundPositionX);
 });
