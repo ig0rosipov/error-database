ddc965363ecd2dadec7a59678f1ce0b79f276bd2
Igor Osipov <igor.osipov@modern.org>
2020-10-19 15:37:08 +0300

    fix: autoresizeble textarea

--- scripts/autosize-textarea.js
+++ scripts/autosize-textarea.js
@@ -1,24 +1,16 @@
 const textareaList = Array.from(document.querySelectorAll('.textarea'));
-
-const resize = () => {
-  textareaList.forEach(item => {
-    item.style.height = 'auto';
-    item.style.height = item.scrollHeight+'px';
-  })
+const resize = (event) => {
+  event.currentTarget.style.height = 'auto';
+  event.currentTarget.style.height = event.currentTarget.scrollHeight + 'px';
 }
 
-const delayedResize = () => {
-  window.setTimeout(resize, 0);
+const delayedResize = (event) => {
+  window.setTimeout(resize(event), 0);
 }
 
 const setResizeListeners = () => {
   textareaList.forEach(item => {
-    item.addEventListener('change', resize, false)
-    item.addEventListener('cut', delayedResize, false)
-    item.addEventListener('paste', delayedResize, false)
-    item.addEventListener('drop', delayedResize, false)
-    item.addEventListener('keydown', delayedResize, false)
-    console.log('click')
+    item.addEventListener('input', (event) => {resize(event);}, false)
   });
 }
 
